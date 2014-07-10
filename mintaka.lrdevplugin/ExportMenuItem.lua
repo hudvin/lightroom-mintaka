e5c3d76584e0cde6e7f5948e42e9f3becaac13e0
hudvin <hudvin@gmail.com>
2014-07-10 21:21:50 +0300

    cleanup

--- mintaka.lrdevplugin/ExportMenuItem.lua
+++ mintaka.lrdevplugin/ExportMenuItem.lua
@@ -1,12 +1,8 @@
 local LrTasks = import 'LrTasks'
--- Access the Lightroom SDK namespaces.
 local LrDialogs = import 'LrDialogs'
-local LrLogger = import 'LrLogger'
 local lrApplication = import 'LrApplication'
 local LrShell = import 'LrShell'
 local LrExportSession = import 'LrExportSession'
-local LrPathUtils = import 'LrPathUtils'
-local LrFileUtils = import 'LrFileUtils'
 
 
 local function getSelection ()
@@ -18,33 +14,16 @@ local function getSelection ()
           end
      end
 
-local function showModalDialog()
-	--local photo=currentPhotos[0]
-    --local path= photo:getRawMetadata( 'path' )
-    --local dateTimeDigitized= photo:getRawMetadata( 'dateTimeDigitized' )
-    --local uuid= photo:getRawMetadata( 'uuid' )
+local function performExport()
 	
 	LrTasks.startAsyncTask(function()
 	  local catalog = lrApplication.activeCatalog()
-
-	  local size = 0
-	  local pathes = ""
-	  local photos = catalog:getMultipleSelectedOrAllPhotos()
 	  local photos = getSelection()
 	  
 	  if next(photos) == nil then
-	     LrDialogs.message("Empty")
+	     LrDialogs.message("Please, select some photos!")
 	     return {}
       end
-	  
-	  local d = "xxx"
-	  for k,photo in ipairs(photos) do
-		  d = photo:getRawMetadata( 'path' )
-		  size = size + photo:getRawMetadata("fileSize")
-		  pathes = pathes .. "\n" .. photo:getRawMetadata("path")
-	  end	
-    
-	  local out = string.format("%.1f MB%s", size / 1024 / 1024, pathes)
 	  
 	  local exportSettings = {
                         LR_format = "JPEG",
@@ -73,33 +52,17 @@ local function showModalDialog()
                        }
       local ex = LrExportSession(params)
       
-	  ex:doExportOnCurrentTask()
+	    ex:doExportOnCurrentTask()
 
       for i, rendition in ex:renditions() do
         local status, other = rendition:waitForRender()
-		
-		local dPath = rendition.destinationPath
-     --   LrDialogs.message(dPath)
-		
---		local ps = LrPathUtils.child("c:/dev/images/", 'photos' )
---		LrDialogs.message(ps)
-	--	LrFileUtils.copy( other, LrPathUtils.child(LrPathUtils.standardizePath(ps), 'qwerty.jpg') )
-		
       end
 	  
-	  
---	  local f = io.open("c:/dev/test_out.txt", "w" )
---	  f:write(d)
- --     f:close()
-	  
-	  LrDialogs.message( "1", d, d)
-	 
 	   
-	 -- LrTasks.execute("notepad.exe")
 	end
 	)
 end
 
-showModalDialog()
+performExport()
 
 
