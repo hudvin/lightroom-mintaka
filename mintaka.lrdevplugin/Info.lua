e49dbb3831a6739790a785af407bad4957f6a67d
hudvin <hudvin@gmail.com>
2014-07-10 21:10:52 +0300

    rename, cleanup

---
+++ mintaka.lrdevplugin/Info.lua
@@ -0,0 +1,23 @@
+return {
+	
+	LrSdkVersion = 3.0,
+	LrSdkMinimumVersion = 1.3, 
+	LrToolkitIdentifier = 'com.nevilon.hudvin.lr.mintaka',
+	LrPluginName = LOC "$$$/Mintaka/PluginName=Mintaka",
+	
+	
+	LrExportMenuItems = {
+		title = "My Cool Plugin",
+		file = "ExportMenuItem.lua",
+	},
+
+	
+	LrLibraryMenuItems = {
+	   {}
+	},
+	VERSION = { major=5, minor=0, revision=0, build=907681, },
+
+}
+
+
+	
