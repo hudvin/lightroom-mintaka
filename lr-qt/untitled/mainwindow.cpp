1d617dc4cebb0c8f514878aee7be1bbd6347bbb5
hudvin <hudvin@gmail.com>
2014-07-12 01:28:16 +0300

    simple table

--- lr-qt/untitled/mainwindow.cpp
+++ lr-qt/untitled/mainwindow.cpp
@@ -10,6 +10,36 @@
 #include <QSize>
 #include <QListView>
 
+#include <QStandardItem>
+#include <QStandardItemModel>
+
+#include <QTableView>
+#include <QTableWidgetItem>
+
+QStandardItemModel* createModel(QObject* parent)
+{
+    const int numRows = 10;
+    const int numColumns = 10;
+
+    QStandardItemModel* model = new QStandardItemModel(numRows, numColumns);
+    model->setHeaderData(0,Qt::Horizontal, "keyword");
+    model->setHeaderData(1,Qt::Horizontal, "number of photos");
+
+
+    for (int row = 0; row < numRows; ++row)
+    {
+        for (int column = 0; column < numColumns; ++column)
+        {
+            QString text = QString('A' + row) + QString::number(column + 1);
+            QStandardItem* item = new QStandardItem(text);
+            model->setItem(row, column, item);
+        }
+     }
+
+    return model;
+}
+
+
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
@@ -21,6 +51,14 @@ MainWindow::MainWindow(QWidget *parent) :
 
     QDirIterator dirIt(ss,QDirIterator::Subdirectories);
     ui->lrImages->setIconSize(QSize(250,250));
+
+    QTableView* keywordsTable = ui->keywordsTable;
+    keywordsTable->setModel(createModel(keywordsTable));
+
+//    QTableWidgetItem *header1 = new QTableWidgetItem();
+//    header1->setText("Switch \"ON\" to Load");
+//    keywordsTable->setH
+//    tableWidget->setHorizontalHeaderItem(0,header1);
 
 
     while (dirIt.hasNext()) {
